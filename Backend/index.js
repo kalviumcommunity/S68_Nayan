557698ecca3a2eab00069419e78f3a7c0c956b25
nayan457-bit <nayan.krishna.s68@kalvium.community>
2025-02-13 15:12:42 +0530

    done

---
+++ Backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { connectDB } = require('./db');  
+const bodyParser = require('body-parser');
+const routes = require('./routes'); 
+
+const app = express();
+
+require('dotenv').config();
+const port = process.env.PORT || 5000;  
+const url = process.env.db_url; 
+
+
+app.use(bodyParser.json());
+
+
+app.listen(port, async () => {
+  try {
+    await connectDB(url);  
+    console.log(`Connected to the database and server is running on port ${port}`);
+  } catch (error) {
+    console.error('Database connection failed:', error);
+  }
+});
+
+// Define CRUD routes
+app.use('/api', routes);  // Use the routes defined in routes.js (prefixed with '/api')
+
+// Root endpoint (just a simple test endpoint)
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
